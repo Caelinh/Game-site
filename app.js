7f15d4efa3e54879fbab01a1b4f89233a8dd84e4
Caelin hargrave <Caelinhargrave@gmail.com>
2022-05-26 16:18:59 -0500

    adding script and images to game site

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-let firstName = prompt("What is your name?");
- 
 
-if (firstName) {
-    document.write("Hello " + firstName + "!");
-} else
-document.write("Please refresh and enter a name");
+let rNum = Math.floor(Math.random()*3);
+
+
+function askName(){
+    let firstName = prompt ("What is your name?") ;
+    
+    if (firstName) {
+        document.write("Hello " + firstName + "!");
+    } else if(!firstName) {
+        firstName = prompt("Please enter your name");
+        document.write("Hello " + firstName + "!");
+    } 
+    }
+
+
+function picChooser() {
+    if (rNum==1){
+        const game1 = "https://cdna.artstation.com/p/assets/images/images/043/897/516/large/seed-seven-twodots-seedseven-eldenring-1.jpg?1638544010"
+        return game1
+    }
+}
